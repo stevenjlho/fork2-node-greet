f01c55c911230821c8d162e7c1dae3fd0340d8e1
stevenjlho <stevenjlho@gmail.com>
2014-07-02 21:54:42 +0800

    添加Mocha框架进行代码测试

---
+++ test/greet_spec.js
@@ -0,0 +1,14 @@
+// var expect = require("chai").expect
+var greet = require("../index.js");
+
+describe('should greet a person by name',function() {
+	it("should greet a person by name", function() {
+		expect(greet('steven')).to.eq("hello steven");
+	});
+});
+
+describe('should greet a person flirtatiously if drunk',function() {
+	it("excute function", function() {
+		expect(greet('steven', 'drunk')).to.eq("hello steven, you look sexy today");
+	});
+});
